26d978e48d2f54e0841dc642d27a7bdd029425ee
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-04-22 16:10:42 +0200

    Exercise1

---
+++ 1 - Create Table/exercise.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+    Books (
+        book_id INTEGER PRIMARY KEY,
+        title TEXT,
+        author TEXT,
+        genre TEXT,
+        published_year DATE,
+        isbn INTEGER,
+        price INTEGER,
+        rating INTEGER,
+        stock_count INTEGER
+    )
