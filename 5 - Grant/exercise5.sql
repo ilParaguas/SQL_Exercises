9237cb7a0f6aaaf8028de930a43001e5fc893fd0
Francisco Javier Martinez Rodriguez <javic9165@gmail.com>
2024-04-22 16:13:55 +0200

    Exercise 5

---
+++ 5 - Grant/exercise5.sql
@@ -0,0 +1,8 @@
+/*
+You've hired an assistant for the bookstore named Martin.
+Grant him permission to view and update the Books table, 
+but he shouldn't be able to delete any records.
+IP address of Martinis martin@localhost .
+*/
+
+GRANT SELECT, INSERT, UPDATE ON Books TO 'martin'@'localhost'
